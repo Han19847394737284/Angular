65000a02f4aa6438a55b34edf75a9c325003fea8
JGCH <jhonathang@hotmail.es>
2018-08-20 20:26:34 -0500

    Angular

---
+++ 02-spa/src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    //template: `<p>I am Component Header</p>`
+    templateUrl: 'header.view.html'
+})
+
+export class HeaderComponent {
+
+}
