2d71c258d3c45236c13f38e532053186aa077803
MacBookPro <macbookpro@MacBookPro.local>
2018-09-29 10:28:09 -0500

    Update training

---
+++ 04-spotiApp/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { SearchComponent } from './component/search/search.component';
+import { ArtistaComponent } from './component/artista/artista.component';
+import { NavbarComponent } from './component/share/navbar/navbar.component';
+
+// Import Routes
+import { ROUTES } from './app.route'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, { useHash: true })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
